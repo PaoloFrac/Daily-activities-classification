ba1d9321ea6c5d3baef363d9bf898cc47aac67c4
Paolo Fraccaro <paolo.fraccaro@manchester.ac.uk>
2018-05-31 10:14:16 +0100

    remove google unncessary code

--- classify_identified_places_osmdata.R
+++ classify_identified_places_osmdata.R
@@ -425,56 +425,5 @@ sf_places_classified_duplicates <- sf_places_classified %>%
 sf_places_classified <- sf_places_classified %>% 
   anti_join(sf_places_classified_duplicates, by = c("patient", "placeID"))
 
-### try to use Google Places API to classify unclassified places
-# key Google Place API
-key= c("AIzaSyDWs7eStEfQRGG8tuNDheo2SJR8ooPjr14",
-       "AIzaSyCdyXaICjKXqefkUUzebnw7A6wDvcQac7",
-       "AIzaSyCl_RMVOmZfOVdfj8Umn9RRytSHdSIIV3k",
-       "AIzaSyAoZSLAoWUxPlOBGE3EeHUlxJ9arrPJt90")
-
-# select unclassified places 
-sf_places_unclassified <- sf_places_classified %>% 
-  filter(is.na(placeType)) %>% 
-    select(patient, placeID) %>% 
-      left_join(sf_places, by = c("patient", "placeID")) # merging with place coordinates
-
-# initialise variable
-sf_places_unclassified_google <- NULL
-
-for(i in 1:nrow(sf_places_unclassified)){
-  
-  tmp <- getGooglePlacesPOI(sf_places_unclassified$CenterLat[i], sf_places_unclassified$CenterLong[i], key)
-  
-  if(is.null(tmp)){ # if nothing was found
-    
-    sf_places_unclassified_google <- bind_rows(sf_places_unclassified_google,
-                                               data.frame(patient = sf_places_unclassified$patient[i],
-                                                          placeID = sf_places_unclassified$placeID[i],
-                                                          placeType = NA_character_)) # add only patient and placeID
-    
-  } else{
-    
-    if(tmp$distance <= distance_threshold){ # only if it respects the distance threshold
-      
-      sf_places_unclassified_google <- bind_rows(sf_places_unclassified_google,
-                                                 data.frame(patient = sf_places_unclassified$patient[i],
-                                                            placeID = sf_places_unclassified$placeID[i],
-                                                            placeType = tmp$placeType))
-      
-    } else{
-      
-      sf_places_unclassified_google <- bind_rows(sf_places_unclassified_google,
-                                                 data.frame(patient = sf_places_unclassified$patient[i],
-                                                            placeID = sf_places_unclassified$placeID[i],
-                                                 placeType = NA_character_))
-      
-    }
-    
-  }
-  
-}
-
-
-
 
 save.image(paste("labelling_results",timeThreshold,".RData"))
