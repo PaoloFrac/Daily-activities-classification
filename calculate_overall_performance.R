a698f0d8f81617c4b0378f555ddcca13f13cbe66
Paolo Fraccaro <paolo.fraccaro@manchester.ac.uk>
2018-08-13 14:01:22 +0100

    add distance threshold

--- calculate_overall_performance.R
+++ calculate_overall_performance.R
@@ -1,6 +1,10 @@
 timeThreshold <- c(10:5)*60
 
+distance_threshold <- 100
+
 analysis_type <- c("time_based", "density_based", "combined")
+
+analysis_type <- c("combined")
 
 for(i in 1:length(analysis_type)){
   
@@ -8,10 +12,14 @@ for(i in 1:length(analysis_type)){
   
   for(j in 1:length(timeThreshold)){
     
-    overall_performance <- read.csv( paste("~/Data/Projects/Club M/Healthy volunteers study/Analysis/",
-                              analysis_type[i],
-                              "/performance_OSM",
-                              timeThreshold[j],".csv", sep = "")) %>% 
+    overall_performance <- read.csv(file = paste("~/Data/Projects/Club M/Healthy volunteers study/Analysis/",
+                                          analysis_type,
+                                          "/performance_OSM", 
+                                          timeThreshold[j],
+                                          "s_",
+                                          distance_threshold,
+                                          ".csv", 
+                                          sep = "")) %>% 
                 select(-X, -patient) %>% 
                   slice(nrow(.)) %>% 
                     mutate(analysis = paste(analysis_type[i],
@@ -25,6 +33,8 @@ for(i in 1:length(analysis_type)){
   
   write.csv(overall_performance, paste("~/Data/Projects/Club M/Healthy volunteers study/Analysis/",
                                        analysis_type[i],
-                                       "/performance_OSM_overall.csv", sep = ""))
+                                       "/performance_OSM_overall_",
+                                       distance_threshold,
+                                       ".csv", sep = ""))
   
 }
